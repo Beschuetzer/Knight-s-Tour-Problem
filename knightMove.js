e67bad4671e4379971560fd9cccbaed62410a96e
Adam <adam.j.major@gmail.com>
2021-01-31 09:23:59 -0800

    No longer infinite loop

--- knightMove.js
+++ knightMove.js
@@ -8,29 +8,37 @@
 //return null if the move is out of bounds
 //return [] if it is the end location
 
-function knightMove (start, end, visited = []) {
+function knightMove (start, end, visited = [], shortestThusFar = {}) {
+    console.log('initial------------------------------------------------');
     console.log('start =', start);
     console.log('end =', end);
     console.log('visited =', visited);
+    console.log('shortestThusFar =', shortestThusFar);
+
     if (getEqual(start, end)) return [];
     if (getAlreadyVisited(start, visited)) return null;
-
-   
+    if (shortestThusFar['shortest'] && shortestThusFar['shortest'].length <= visited.length) return null;
     
     const possibleMoves = getPossibleMoves(start);
-    if (getAlreadyVisited(end, possibleMoves)) return [start, ...visited, end];
+    if (getAlreadyVisited(end, possibleMoves)) return [...visited];
 
 
     let shortest = null;
     console.log('possibleMoves =', possibleMoves);
     for(move of possibleMoves) {
-        const result = knightMove(move, end, [...visited, start]);
+        let result = knightMove(move, end, [...visited, start], shortestThusFar);
         console.log('result =', result);
         if (result !== null) {
-            result.push(move);
-            if (shortest === null || shortest.length > result) shortest = result;
+            result = [...result, move, end];
+            if (shortest === null || shortest.length > result) {
+                shortest = result;
+                if (!shortestThusFar['shortest'] || shortestThusFar['shortest'] && shortest.length < shortestThusFar['shortest'].length) shortestThusFar['shortest'] = shortest;
+            }
+
         }
     }
+
+
     return shortest;
 }
 
@@ -55,16 +63,16 @@ function getAlreadyVisited(start, visited) {
     return false;
 }
 function getEqual(arr1, arr2) {
-    console.log('getEqual------------------------------------------------');
-    console.log('arr1 =', arr1);
-    console.log('arr2 =', arr2);
+    // console.log('getEqual------------------------------------------------');
+    // console.log('arr1 =', arr1);
+    // console.log('arr2 =', arr2);
     if (arr1[0] === arr2[0] && arr1[1] === arr2[1]) return true;
     return false;
 }
 
 
-// console.log(knightMove([4,4],[5,2]))   //[5,2]
-console.log(knightMove([0,0],[0,4]))   //[[0,0],[1,2]]
+console.log(knightMove([4,4],[5,2]))   //[5,2]
+// console.log(knightMove([0,0],[4,0]))   //[[0,0],[1,2]]
 // console.log(knightMove([0,0],[1,0]))   //[[1,2],[3,1],[0,1]]
 
 
